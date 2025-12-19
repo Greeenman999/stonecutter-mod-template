e7caf452ce42828d5560e66ec68cb3a34a5de3b7
Greenman999 <git@greenman999.de>
2025-12-19 16:46:28 +0100

    feat: include LICENSE file in jars

--- build-logic/src/main/kotlin/ModPlatformPlugin.kt
+++ build-logic/src/main/kotlin/ModPlatformPlugin.kt
@@ -14,6 +14,7 @@ import org.gradle.api.JavaVersion
 import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.file.DuplicatesStrategy
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Copy
@@ -100,6 +101,11 @@ abstract class ModPlatformPlugin @Inject constructor() : Plugin<Project> {
 	private fun Project.configureJarTask(modId: String) {
 		tasks.withType<Jar>().configureEach {
 			archiveBaseName.set(modId)
+			duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+			from(rootProject.file("LICENSE")) {
+				rename { "${it}_${modId}" }
+			}
 		}
 	}
 
