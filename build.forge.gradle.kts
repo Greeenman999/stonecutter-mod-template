3d9b2b8d57dc340b3d5f111855861cd5d90e4515
Greenman999 <git@greenman999.de>
2025-12-19 16:52:46 +0100

    fix: update copyright notices to include stonecutter-mod-template contributors and fix rotgrunegelb typo

--- build.forge.gradle.kts
+++ build.forge.gradle.kts
@@ -1,7 +1,7 @@
 /*
  * SPDX-License-Identifier: MIT
- * Copyright (c) 2025 rotgrunegelb
- * Copyright (c) 2025 murder_spagurder (from murderspagurder/mod-template-java)
+ * Copyright (c) 2025 rotgruengelb, and stonecutter-mod-template contributors
+ * Copyright (c) 2025 murder_spagurder
  * See the LICENSE file in the project root for license terms.
  */
 import org.gradle.kotlin.dsl.register
